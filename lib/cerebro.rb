7ccbc52b52d1a8b2b82f4be6fb9d2944cb1c0bd8
James Wen <jrw2175@columbia.edu>
2016-06-17 13:35:48 -0400

    Use CLAide for command line arguments and options parsing

--- lib/cerebro.rb
+++ lib/cerebro.rb
@@ -1,9 +1,35 @@
 require "cerebro/version"
+require 'claide'
 
 module Cerebro
   class << self
     def fork_search
-      puts "Fork Search Here"
+    end
+  end
+
+  class CLI
+    COMMANDS = %w(search)
+    def self.start(argv, options = {})
+      argv = CLAide::ARGV.new(argv)
+      command = argv.shift_argument
+      exit 1 unless Cerebro::CLI::COMMANDS.include?(command)
+
+      if command == "search"
+        Cerebro::Commands::Search.run(argv)
+      end
+    end
+  end
+
+  module Commands
+    class Search < CLAide::Command
+      def initialize(argv)
+	@owner = argv.shift_argument
+	@repo = argv.shift_argument
+	super
+      end
+      def run
+	puts "About to search through forks for #{@owner}/#{@repo}"
+      end
     end
   end
 end
